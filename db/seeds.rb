1a2819e62d80c035bd9376d07da0531254d15214
Hugo Mariou <mariou.hugo@gmail.com>
2022-08-10 16:15:32 -0300

    routes made and such

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning DB...'
+Restaurant.destroy_all
+
+puts 'Seeding restaurants...'
+
+5.times do
+  restau = Restaurant.create!(
+    name: Faker::JapaneseMedia::DragonBall.character,
+    address: Faker::Games::ElderScrolls.region,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+end
